a7ab03aa77b5f91369a80a713d4d990676a102d9
Azhe Kun <azhe403@gmail.com>
2021-07-29 22:07:57 +0700

    update based ep. 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,11 +6,13 @@
     <article>
         <h2 class="mb-5">{{ $post->title }}</h2>
 
-        <p>By. Azhe-kun in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+        <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in
+            <a class="text-decoration-none"
+               href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
 
         <h5>{{ $post->author }}</h5>
         <p>{{ $post->body }}</p>
     </article>
 
-    <a href="/posts">Back</a>
+    <a class="d-block mt-5" href="/posts">Back</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,16 +8,21 @@
     @foreach($posts as $post)
         <div>
 
-            <article class="mb-5">
+            <article class="mb-5 border-bottom pb-3">
                 <h2>
-                    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
                 </h2>
-                <h5>{{ $post->author }}</h5>
+                <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in
+                    <a class="text-decoration-none"
+                       href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                </p>
+
+                </h5>
+
                 <p>{{ $post->excerpt }}</p>
 
-                <a href="/posts">
-                    <button class="btn btn-primary">Delete</button>
-                </a>
+                <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read more..</a>
+                <a href="/posts" class="btn btn-primary">Delete</a>
             </article>
         </div>
 
